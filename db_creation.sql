7090334ad7d2d9a2f776f41242f615d94aa15964
HakiEBIBI <haki.ebibi@jobtrek.ch>
2025-04-01 14:06:50 +0200

    feat: created database close: #2

---
+++ db_creation.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS blog_posts;
+DROP TABLE IF EXISTS user;
+
+create table user
+(
+    id       integer primary key autoincrement unique,
+    name     varchar(50),
+    email    varchar(50) unique not null,
+    password varchar not null
+);
+
+
+create table blog_posts
+(
+    user_id    integer,
+    title      varchar not null,
+    image      text,
+    content    varchar not null,
+    created_at text not null,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
