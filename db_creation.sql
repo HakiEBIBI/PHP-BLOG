b04e525afafb6a8e6e5976790e1a18c8c988f352
HakiEBIBI <haki.ebibi@jobtrek.ch>
2025-04-11 10:56:09 +0200

    feat: added a way that all the blog is affiched in the home

--- db_creation.sql
+++ db_creation.sql
@@ -13,10 +13,17 @@ create table user
 create table blog_posts
 (
     id       integer primary key autoincrement unique,
-    user_id    integer,
+    user_id    integer not null ,
     title      varchar not null,
     image      text,
     content    varchar not null,
     created_at text not null,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+INSERT INTO user (name, email, password)
+VALUES ('John Doe', 'JohnDoe@gmail.com', '$2y$10$fAiGjjnVTUBq4cIxBBC.rOJhVuAE74tTrjethCfoqtSUuBR6TKehu');
+
+INSERT INTO user (name, email, password)
+VALUES ('haki', 'haki@gmail.com', '$2y$10$6mz2LQda741vqD2kDOXmfu9KVBeHMyFsgNjqihjPtZPmRLsh0dQIG');
+
