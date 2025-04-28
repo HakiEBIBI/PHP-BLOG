0b150e53ca4986ab1671776138d4dcae46f522b6
HakiEBIBI <haki.ebibi@jobtrek.ch>
2025-04-28 14:19:47 +0200

    refactor: removed the dead code in test-if-admin.php

--- public/test-if-admin.php
+++ public/test-if-admin.php
@@ -1,12 +1,4 @@
 <?php
-$db = 'sqlite:../Database.db';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-$PDO = new PDO($db, '', '', $options);
-
 function isUserAdmin($PDO, $userId) {
     $stmt = $PDO->prepare('SELECT is_admin FROM user WHERE id = ?');
     $stmt->execute([$userId]);
